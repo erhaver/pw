c30dad7b97309a6b0a6d1d789bc8eff9602e850f
erhaver <erhaver@gmail.com>
2022-11-13 21:47:52 +0300

    Update pw.sh
    Temporary file

--- pw.sh
+++ pw.sh
@@ -3,11 +3,17 @@
 #
 # Generate password without specific symbols:
 intPW=$(pwgen -syvB -1 16 -r \`\"\~\!\@\:\'\,\.\+\(\)\;\{\}\=\-\_\\\<\>)
+
+#TODO: 
+# 1. WSL clipboard
+# 2. wsl-notify-send 
+
+
 # Copy and print password.
 # Uncomment next line if you want it:
 #echo "$intPW" | xclip -filter -selection clipboard
 # Copy but don't print password:
-echo "$intPW" | xclip -selection clipboard
+# echo "$intPW" | xclip -selection clipboard
 # Send notify.
 # Change icon if you want:
-notify-send "Пароль скопирован" -i '/usr/share/icons/Adwaita/32x32/actions/edit-copy-symbolic.symbolic.png' 
+# notify-send "Пароль скопирован" -i '/usr/share/icons/Adwaita/32x32/actions/edit-copy-symbolic.symbolic.png' 
