466d8f1a6a059fe60cd849a7da571e21118bf6b4
erhaver <erhaver@gmail.com>
2022-11-13 21:31:04 +0300

    Create pw.sh

---
+++ pw.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# pwgen should be installed.
+#
+# Generate password without specific symbols:
+intPW=$(pwgen -syvB -1 16 -r \`\"\~\!\@\:\'\,\.\+\(\)\;\{\}\=\-\_\\\<\>)
+# Copy and print password.
+# Uncomment next line if you want it:
+#echo "$intPW" | xclip -filter -selection clipboard
+# Copy but don't print password:
+echo "$intPW" | xclip -selection clipboard
+# Send notify.
+# Change icon if you want:
+notify-send "Пароль скопирован" -i '/usr/share/icons/Adwaita/32x32/actions/edit-copy-symbolic.symbolic.png' 
