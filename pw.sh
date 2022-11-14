b12c260988582c3e0d59bfcd87188dd38e619c2a
pbx-profi <33713952+pbx-profi@users.noreply.github.com>
2022-11-14 12:19:16 +0300

    Update pw.sh
    WSL clipboard

--- pw.sh
+++ pw.sh
@@ -3,17 +3,12 @@
 #
 # Generate password without specific symbols:
 intPW=$(pwgen -syvB -1 16 -r \`\"\~\!\@\:\'\,\.\+\(\)\;\{\}\=\-\_\\\<\>)
+echo $intPW | clip.exe
+
 
 #TODO: 
-# 1. WSL clipboard
+# 1. WSL clipboard | done
 # 2. wsl-notify-send 
 
-
-# Copy and print password.
-# Uncomment next line if you want it:
-#echo "$intPW" | xclip -filter -selection clipboard
-# Copy but don't print password:
-# echo "$intPW" | xclip -selection clipboard
-# Send notify.
-# Change icon if you want:
+# Send notify on linux:
 # notify-send "Пароль скопирован" -i '/usr/share/icons/Adwaita/32x32/actions/edit-copy-symbolic.symbolic.png' 
