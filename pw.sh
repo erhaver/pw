204259240e4863e8b44a4413fd15c76adced1eac
erhaver <erhaver@gmail.com>
2022-11-14 20:17:11 +0300

    Update pw.sh

--- pw.sh
+++ pw.sh
@@ -4,11 +4,4 @@
 # Generate password without specific symbols:
 intPW=$(pwgen -syvB -1 16 -r \`\"\~\!\@\:\'\,\.\+\(\)\;\{\}\=\-\_\\\<\>)
 echo $intPW | clip.exe
-
-
-#TODO: 
-# 1. WSL clipboard | done
-# 2. wsl-notify-send 
-
-# Send notify on linux:
-# notify-send "Пароль скопирован" -i '/usr/share/icons/Adwaita/32x32/actions/edit-copy-symbolic.symbolic.png' 
+echo "Пароль скопирован!"
